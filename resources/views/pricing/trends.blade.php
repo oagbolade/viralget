673913784e139d8efad1b2523e7d449a86ea21a6
oagbolade <dayoagbolade@gmail.com>
2020-07-02 20:36:19 +0100

    Feat: new pricing pages

--- resources/views/pricing/trends.blade.php
+++ resources/views/pricing/trends.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+  @push('pricingStyles')
+      <link href="{{ asset('css/pricing.css') }}" rel="stylesheet">
+  @endpush
    <!-- Header -->
    <header class="header bg-gray" style="background: #fafbfb url('/images/social.png') 800px bottom no-repeat">
       <div class="container">
@@ -23,111 +26,35 @@
       | Pricing
       |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
       !-->
+    <p class="lead-5 text-center text-dark fw-600 lh-2 mb-5">Select a pricing plan</p>
+    <section class="pricing py-5">
+      <div class="container">
 
-
-      <section class="section">
-        <div class="container">
-              <p class="lead-5 text-center text-dark fw-600 lh-2 mb-5">Select a pricing plan</p>
-
-          <div class="row gap-y align-items-center">
-          @include('layouts.partials.status')
-
-          
-            <div class="col-md-4">
-              <div class="pricing-1">
-                <p class="plan-name">Top 10</p>
-                <br>
-                {{-- <h2 class="price">N457,950</h2> --}}
-                <h2 class="price">
+        <div class="row">
+          <!-- Free Tier -->
+          <div class="col-lg-4">
+            <div class="card mb-5 mb-lg-0">
+              <div class="card-body">
+                <h5 class="card-title text-muted text-uppercase text-center">TOP 10</h5>
+                <h6 class="card-price text-center">
                   <span class="price-unit">&#8358;</span>
-                  <span data-bind-radio="pricing" data-monthly="9" data-yearly="99">457,950</span>
-                  <span class="plan-period" data-bind-radio="pricing" data-monthly="/mo" data-yearly="/yr">/mo</span>
-                </h2>
-                <p class="small">&nbsp;</p>
-
-                <div class="text-muted">
-                  <small>Minimum of 20 Influencers</small><br>
-                  <small>Influencer Profiling</small><br>
-                  <small>Content Creation/Tweetbank</small><br>
-                  <small>Up to 200 Tweets</small><br>
-                  <small>2/3 Hours on Trending Topics</small><br>
-                  <small>Influencer Management</small><br>
-                  <small>Comprehensive Report</small><br><br><br><br>
-                </div>
-
-                <br>
+                  <span data-bind-radio="pricing" data-monthly="39" data-yearly="399">457,950</span>
+                  <span class="period">/month</span>
+                </h6>
+                <hr>
+                <ul class="fa-ul">
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Minimum of 20 Influencers</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Influencer Profiling</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Content Creation/Tweetbank</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Up to 200 Tweets</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>2/3 Hours on Trending Topics</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Influencer Management</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Comprehensive Report</li>
+                  {{-- Negative sample --}}
+                  {{-- <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li> --}}
+                </ul>
                 <p class="text-center py-3">
-                @if($user && !$user->subscription) 
-                  <a class="btn btn-primary" href="{{ route('subscribe', ['plan' => 'free'])}}" data-bind-href="pricing" data-monthly="#monthly" data-yearly="#yearly">                
-                    Get Started
-                   </a>
-                  @endif
-                </p>
-              </div>
-            </div>
-
-
-            <div class="col-md-4">
-              <div class="pricing-1 popular">
-                <p class="plan-name">Top 5</p>
-                <br>
-                <h2 class="price text-success">
-                  <span class="price-unit">&#8358;</span>
-                  <span data-bind-radio="pricing" data-monthly="9" data-yearly="99">15k</span>
-                  <span class="plan-period" data-bind-radio="pricing" data-monthly="/mo" data-yearly="/yr">/mo</span>
-                </h2>
-                <p class="small">&nbsp;</p>
-
-                <div class="text-muted">
-                  <small>Minimum of 30 Influencers</small><br>
-                  <small>Influencer Profiling</small><br>
-                  <small>Content Creation/Tweetbank</small><br>
-                  <small>Up to 300 Tweets</small><br>
-                  <small>3 Hours on Trending Topics</small><br>
-                  <small>Influencer Management</small><br>
-                  <small>Comprehensive Report</small><br>
-                  
-                </div>
-
-                <br>
-                <p class="text-center py-3">
-                <a class="btn btn-success" href="{{ route('subscribe', ['plan' => 'premium'])}}" data-bind-href="pricing" data-monthly="#monthly" data-yearly="#yearly">                
-                  @if($user && $user->subscription && $user->subscription->plan->name == 'Free') 
-                    Upgrade 
-                  @else
-                    Get Started
-                  @endif
-                  </a>
-                </p>
-              </div>
-            </div>
-
-
-            <div class="col-md-4">
-              <div class="pricing-1">
-                <p class="plan-name">Number 1</p>
-                <br>
-                <h2 class="price">
-                  <span class="price-unit">&#8358;</span>
-                  <span data-bind-radio="pricing" data-monthly="39" data-yearly="399">40k</span>
-                  <span class="plan-period" data-bind-radio="pricing" data-monthly="/mo" data-yearly="/yr">/mo</span>
-                </h2>
-                <p class="small">&nbsp;</p>
-
-                <div class="text-muted">
-                  <small>Minimum of 40 Influencers</small><br>
-                  <small>Influencer Profiling</small><br>
-                  <small>Content Creation/Tweetbank</small><br>
-                  <small>Up to 400 Tweets</small><br>
-                  <small>3 Hours on Trending Topics</small><br>
-                  <small>Influencer Management</small><br>
-                  <small>Comprehensive Report</small><br>
-                  
-                </div>
-
-                <br>
-                <p class="text-center py-3">
-                  <a class="btn btn-primary" href="{{ route('subscribe', ['plan' => 'enterprise'])}}" data-bind-href="pricing" data-monthly="#monthly" data-yearly="#yearly">
+                  <a class="btn btn-block btn-primary text-uppercase" href="{{ route('subscribe', ['plan' => 'enterprise'])}}" data-bind-href="pricing" data-monthly="#monthly" data-yearly="#yearly">
                   @if($user && $user->subscription && $user->subscription->plan->name != 'enterprise') 
                     Upgrade 
                   @else
@@ -137,13 +64,72 @@
                 </p>
               </div>
             </div>
-
           </div>
-
+          <!-- Plus Tier -->
+          <div class="col-lg-4">
+            <div class="card mb-5 mb-lg-0">
+              <div class="card-body">
+                <h5 class="card-title text-muted text-uppercase text-center">TOP 5</h5>
+                <h6 class="card-price text-center">
+                  <span class="price-unit">&#8358;</span>
+                  <span data-bind-radio="pricing" data-monthly="39" data-yearly="399">580,500</span>
+                  <span class="period">/month</span>
+                </h6>            <hr>
+                <ul class="fa-ul">
+                <li><span class="fa-li"><i class="fa fa-check"></i></span>Minimum of 30 Influencers</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Influencer Profiling</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Content Creation/Tweetbank</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Up to 300 Tweets</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>2/3 Hours on Trending Topics</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Influencer Management</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Comprehensive Report</li>
+                </ul>
+                <p class="text-center py-3">
+                  <a class="btn btn-block btn-warning text-uppercase" href="{{ route('subscribe', ['plan' => 'enterprise'])}}" data-bind-href="pricing" data-monthly="#monthly" data-yearly="#yearly">
+                  @if($user && $user->subscription && $user->subscription->plan->name != 'enterprise') 
+                    Upgrade 
+                  @else
+                    Get Started
+                  @endif
+                  </a>
+                </p>            
+              </div>
+            </div>
+          </div>
+          <!-- Pro Tier -->
+          <div class="col-lg-4">
+            <div class="card mb-5 mb-lg-0">
+              <div class="card-body">
+                <h5 class="card-title text-muted text-uppercase text-center">NUMBER 1</h5>
+                <h6 class="card-price text-center">
+                  <span class="price-unit">&#8358;</span>
+                  <span data-bind-radio="pricing" data-monthly="39" data-yearly="399">864,300</span>
+                  <span class="period">/month</span>
+                </h6>            <hr>
+                <ul class="fa-ul">
+                <li><span class="fa-li"><i class="fa fa-check"></i></span>Minimum of 40 Influencers</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Influencer Profiling</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Content Creation/Tweetbank</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Up to 400 Tweets</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>3 Hours on Trending Topics</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Influencer Management</li>
+                  <li><span class="fa-li"><i class="fa fa-check"></i></span>Comprehensive Report</li>
+                </ul>
+                <p class="text-center py-3">
+                  <a class="btn btn-block btn-success text-uppercase" href="{{ route('subscribe', ['plan' => 'enterprise'])}}" data-bind-href="pricing" data-monthly="#monthly" data-yearly="#yearly">
+                  @if($user && $user->subscription && $user->subscription->plan->name != 'enterprise') 
+                    Upgrade 
+                  @else
+                    Get Started
+                  @endif
+                  </a>
+                </p>            
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-
-
+      </div>
+    </section>
 </main>
 
 @endsection
