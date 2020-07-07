18f58bf844a1eef44023d8f8e0e1068d56b4ea2a
oagbolade <dayoagbolade@gmail.com>
2020-07-07 20:22:32 +0100

    Feat: added pricing restriction to campaign page

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -8,6 +8,11 @@ class CampaignController extends Controller
 {
     function index()
     {
+        $user = Auth()->user();
+
+        if(!$user->subscription){
+            return redirect()->to('/pricing')->withErrors(['error' => 'Please select a pricing plan to continue']);
+        }
         return view('campaigns.view');
     }
    
