70cd2a2dd0789a25e883c620dc1d2d53f0e773cb
oagbolade <dayoagbolade@gmail.com>
2020-07-19 18:03:24 +0100

    Added success session

--- resources/views/managements/checkout.blade.php
+++ resources/views/managements/checkout.blade.php
@@ -4,96 +4,105 @@
 
 <header class="header bg-gray">
     <div class="container">
-    <div class="row h-10">
+        <div class="row h-10">
 
-        <h4>Checkout</h4>
+            <h4>Checkout</h4>
 
-    </div>
+        </div>
     </div>
 </header>
 
-   <!-- Main Content -->
-   <main class="main-content">
+<!-- Main Content -->
+<main class="main-content">
 
     <section class="section">
-    <div class="container">
+        <div class="container">
 
-        <div class="row gap-y">
-        <div class="col-lg-8">
+            <div class="row gap-y">
+                <div class="col-lg-8">
+                    <div>
+                        @if(session('success'))
+                        <div class="alert alert-success" role="alert">
+                            <h3>{{session('success')}}</h3>
+                        </div>
+                        @endif
+                    </div>
+                    <table class="table table-cart">
+                        <tbody valign="middle">
 
-            <table class="table table-cart">
-            <tbody valign="middle">
+                            <tr>
+                                <td>
+                                    <div class="{{ $plan->color }} text-white text-center lead p-3">{{ $plan->name }}
+                                    </div>
+                                </td>
 
-                <tr>
-                <td>
-                    <div class="{{ $plan->color }} text-white text-center lead p-3">{{ $plan->name }}</div>
-                </td>
+                                <td>
+                                    <h5>ViralGet Subscription</h5>
+                                    <p>{{ $plan->name }} plan</p>
+                                </td>
 
-                <td>
-                    <h5>ViralGet Subscription</h5>
-                    <p>{{ $plan->name }} plan</p>
-                </td>
-
-                <td>
-                    <h4 class="price">{!! $plan->currencyAmount !!}</h4>
-                </td>
-                </tr>
+                                <td>
+                                    <h4 class="price">{!! $plan->currencyAmount !!}</h4>
+                                </td>
+                            </tr>
 
 
 
-            </tbody>
-            </table>
+                        </tbody>
+                    </table>
+
+
+
+                </div>
+
+
+                <div class="col-lg-4">
+                    <div class="cart-price">
+                        <div class="flexbox">
+                            <div>
+                                <p><strong>Subtotal:</strong></p>
+                            </div>
+
+                            <div>
+                                <p>{!! $plan->currencyAmount !!}</p>
+                            </div>
+                        </div>
+
+                        <hr>
+
+                        <div class="flexbox">
+                            <div>
+                                <p><strong>Total:</strong></p>
+                            </div>
+
+                            <div>
+                                <p class="fw-600">{!! $plan->currencyAmount !!}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-12">
+                            <button class="btn btn-block btn-primary" onclick="payWithPaystack()" type="button">Checkout
+                                <i class="ti-angle-right fs-9"></i></button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 
 
 
         </div>
-
-
-        <div class="col-lg-4">
-            <div class="cart-price">
-            <div class="flexbox">
-                <div>
-                <p><strong>Subtotal:</strong></p>
-                </div>
-
-                <div>
-                <p>{!! $plan->currencyAmount !!}</p>
-                </div>
-            </div>
-
-            <hr>
-
-            <div class="flexbox">
-                <div>
-                <p><strong>Total:</strong></p>
-                </div>
-
-                <div>
-                <p class="fw-600">{!! $plan->currencyAmount !!}</p>
-                </div>
-            </div>
-            </div>
-
-            <div class="row">
-
-            <div class="col-12">
-                <button class="btn btn-block btn-primary" onclick="payWithPaystack()" type="button">Checkout <i class="ti-angle-right fs-9"></i></button>
-            </div>
-            </div>
-
-        </div>
-        </div>
-
-
-
-    </div>
     </section>
 
-    </main>
+</main>
 @endsection
 @section('scripts')
 <script src="https://js.paystack.co/v1/inline.js"></script>
 <script>
+    window.location = '/subscribe/management/confirm/'+ response.reference;
   function payWithPaystack(){
     var handler = PaystackPop.setup({
       key: '{{ env("PAYSTACK_PK") }}',
@@ -110,10 +119,7 @@
          ]
       },
       callback: function(response) {
-          // Send mail 
-          // Authenticate user
-          // Go to dashboard
-          window.location = '/subscribe/confirm/'+ response.reference;
+          window.location = '/subscribe/management/confirm/'+ response.reference;
       },
     });
     handler.openIframe();
