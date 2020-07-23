282d835eb84a0dc447762cf36ac8bdbe11e1f156
oagbolade <dayoagbolade@gmail.com>
2020-07-23 12:25:38 +0100

    Added blocker for 24hrs after trend date

--- resources/js/components/campaigns/management/Trends.vue
+++ resources/js/components/campaigns/management/Trends.vue
@@ -117,7 +117,17 @@
                   {{ campaign.paid === "false" ? "No" : "Yes" }}
                 </button>
                 <div v-if="campaign.paid === 'false'">
-                  <u @click="goToCheckout(campaign.booking_type, campaign.plan_id, campaign.email, campaign.id)"><a href="#">complete payment</a></u>
+                  <u
+                    @click="
+                      goToCheckout(
+                        campaign.booking_type,
+                        campaign.plan_id,
+                        campaign.email,
+                        campaign.id
+                      )
+                    "
+                    ><a href="#">complete payment</a></u
+                  >
                 </div>
               </td>
               <td>{{ campaign.expired === "false" ? "No" : "Yes" }}</td>
@@ -125,12 +135,14 @@
               <td>
                 <button
                   @click="
-                    viewCampaign(
-                      campaign.user_query,
-                      campaign.trends_plan.id,
-                      campaign.expired,
-                      campaign.paid
-                    )
+                    viewCampaign({
+                      time: campaign.time,
+                      date: campaign.date,
+                      keyword: campaign.user_query,
+                      plan_id: campaign.plan_id,
+                      expired: campaign.expired,
+                      paid: campaign.paid
+                    })
                   "
                   type="button"
                   class="btn btn-label btn-success"
@@ -229,8 +241,8 @@ export default {
     this.getUserCampaigns();
   },
   methods: {
-    goToCheckout(booking_type, plan_id, email, user_plan_id){
-      const URL = `/checkout/${booking_type}/${plan_id}?email=${email}&user_plan_id=${user_plan_id}`
+    goToCheckout(booking_type, plan_id, email, user_plan_id) {
+      const URL = `/checkout/${booking_type}/${plan_id}?email=${email}&user_plan_id=${user_plan_id}`;
       window.location = URL;
     },
     formatCampaignDates(dates) {
@@ -280,18 +292,14 @@ export default {
       }
     },
 
-    viewCampaign(keyword, plan_id, expired, paid) {
-      if (expired === "true") {
-        Swal.fire(
-          "Oops!",
-          "It seems your trend plan has expired. Kindly checkout our pricing page" +
-            " and select a plan that best suits your needs",
-          "question"
-        );
-        return;
-      }
+    viewCampaign(data) {
+      let user_date = moment(data.date);
+      let now = moment();
+      let viewDate = moment(data.date)
+        .add(1, "d")
+        .format("DD-MM-YYYY");
 
-      if (paid === "false") {
+      if (data.paid === "false") {
         Swal.fire(
           "Oops!",
           "It seems you haven't completed your payment," +
@@ -301,9 +309,28 @@ export default {
         return;
       }
 
+      if (now.diff(user_date, "days") < 1) {
+        Swal.fire(
+          "Sorry!",
+          `You can start viewing reports 24hrs after your selected trend date. Please check in again on ${viewDate}`,
+          "question"
+        );
+        return;
+      }
+
+      if (data.expired === "true") {
+        Swal.fire(
+          "Oops!",
+          "It seems your trend plan has expired. Kindly checkout our pricing page" +
+            " and select a plan that best suits your needs",
+          "question"
+        );
+        return;
+      }
+
       const URL = `/management/hashtag?q=${encodeURIComponent(
-        keyword
-      )}&fromDate=&toDate=&plan_id=${plan_id}`;
+        data.keyword
+      )}&fromDate=&toDate=&plan_id=${data.plan_id}`;
       window.location.href = URL;
     },
 
