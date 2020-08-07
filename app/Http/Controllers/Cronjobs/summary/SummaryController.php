1e4e396bb79f27a47e47c52eba0dd6df8044ca3e
mac <mac@macs-MacBook-Air-3.local>
2020-08-07 18:43:52 +0100

    Summary cron controller bug fix

--- app/Http/Controllers/Cronjobs/summary/SummaryController.php
+++ app/Http/Controllers/Cronjobs/summary/SummaryController.php
@@ -20,48 +20,37 @@ class SummaryController extends Controller
 
         foreach ($refresh as $schedule) {
             if ($this->refresh($schedule['last_refresh'])) {
-                $get_user_details = UserDetailsManagement::where(['id' => $schedule['user_details_id']])->get();
+                $get_user_details = UserDetailsManagement::where(['id' => $schedule['user_details_id']])->first();
 
-                foreach ($get_user_details as $user) {
-                    $user_id = $user['user_id'];
-                    $influencers = $user['influencers'];
-                    $keyword = $user['user_query'];
-                    $plan_id = $user['plan_id'];
-                    $user_details_id = $user['user_details_id'];
+                $user_id = $get_user_details->user_id;
+                $influencers = $get_user_details->influencers;
+                $keyword = $get_user_details->user_query;
+                $plan_id = $get_user_details->plan_id;
+                $user_details_id = $get_user_details->id;
 
-                    $twitterAPI = new ManagementTwitterAPIController;
+                $twitterAPI = new ManagementTwitterAPIController;
 
-                    try{
-                        $twitterAPI->getCampaignSummary($user_id, $influencers, $keyword, $plan_id, $user_details_id);
-                    }catch(Exception $e){
-                        continue;
-                    }
-
-                    $new_refresh_date = date("Y-m-d");
-
-                    $this->updateScheduler($user_details_id, $new_refresh_date);
+                try {
+                    $twitterAPI->getCampaignSummary($user_id, $influencers, $keyword, $plan_id, $user_details_id);
+                } catch (Exception $e) {
+                    continue;
                 }
+
+                $new_refresh_date = date("Y-m-d");
+
+                $this->updateScheduler($user_details_id, $new_refresh_date);
             }
         }
-        return 'done';
     }
 
-    public function updateScheduler($user_details_id, $new_refresh_date){
+    public function updateScheduler($user_details_id, $new_refresh_date)
+    {
         try {
             Scheduler::where([
                 'user_details_id' => $user_details_id,
             ])->update([
                 'last_refresh' => $new_refresh_date,
             ]);
-            // Send mail to oladayo
-            // the message
-            $msg = "We hit the cron";
-
-            // use wordwrap() if lines are longer than 70 characters
-            $msg = wordwrap($msg, 70);
-
-            // send email
-            mail("oladayo.agbolade@viralget.com.ng", "Cron Ran", $msg);
         } catch (Exception $e) {
             return response([
                 'status' => 500,
