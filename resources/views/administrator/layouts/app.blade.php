b517ef4160c73504e70aaff966baa661f8529df8
oagbolade <dayoagbolade@gmail.com>
2020-09-06 18:15:26 +0100

    Layout and UI update

--- resources/views/administrator/layouts/app.blade.php
+++ resources/views/administrator/layouts/app.blade.php
@@ -35,14 +35,9 @@
             @include('administrator.layouts.footer')
         </div>
     </div>
-    <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('administrator/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('administrator/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Core plugin JavaScript-->
     <script src="{{ asset('administrator/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
     <script src="{{ asset('administrator/js/sb-admin-2.min.js') }}"></script>
 </body>
 @yield('scripts')
