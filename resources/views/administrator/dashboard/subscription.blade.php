232cf4524227d82d11cb841da8f64080e4cdbd8d
oagbolade <dayoagbolade@gmail.com>
2020-09-06 18:35:47 +0100

    Update assets directory

--- resources/views/administrator/dashboard/subscription.blade.php
+++ resources/views/administrator/dashboard/subscription.blade.php
@@ -8,7 +8,7 @@
 
 @section('scripts')
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('administrator/vendor/chart.js/Chart.min.js') }}"></script>
-<script src="{{ asset('administrator/js/demo/chart-area-demo.js') }}"></script>
-<script src="{{ asset('administrator/js/demo/chart-pie-demo.js') }}"></script>
+<script src="{{ asset('static/assets/administrator/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('static/assets/administrator/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('static/assets/administrator/js/demo/chart-pie-demo.js') }}"></script>
 @endsection
